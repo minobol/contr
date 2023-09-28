1599dd1892e06ae1f7c5bccb854b4d1d02b3842c
minobol <120034142+minobol@users.noreply.github.com>
2023-09-28 14:31:33 +0300

    Update контрольная.cs

--- контрольная.cs
+++ контрольная.cs
@@ -1,42 +1,25 @@
-Console.Write(«Введите количество элементов массива: «);
-int m = Convert.ToInt32(Console.ReadLine());
-string [] stringArray = new string [m];
-void array(string [] stringArray)
+using System;
+
+class Program
 {
-  for (int i = 0;i<stringArray.Length;i++)
-  {
-     Console.WriteLine($»Введите {i+1} элемент массива»);
-     stringArray[i] = Console.ReadLine();
-  }
-}
-string [] symbol(string [] stringArray)
-{
-  int n = 0;
-  for (int i = 0;i<stringArray.Length;i++)
-  {
-    if(stringArray[i].Length <=3)
-    n++;
-  }
-  string [] rez = new string [n];
-  int j = 0;
-  for (int i = 0;i<stringArray.Length;i++)
-  {
-    if(stringArray[i].Length <=3)
+    static void Main(string[] args)
     {
-        rez[j] = stringArray[i];
-        j++;
+        string[] array = { "hello", "world", "this", "is", "a", "test" };
+        string[] newArray = new string[array.Length];
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
     }
-  }
-  return rez;
 }
-void printA(string [] stringArray)
-{
-    Console.Write(«[«);
-    for (int i = 0;i<stringArray.Length;i++)
-    {
-    Console.Write($»‘{stringArray[i]}’, «);
-    }
-    Console.Write(«]»);
-}
-array(stringArray);
-printA(symbol(stringArray));
