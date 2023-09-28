d4cfb8059cd19874ac4b6076ffd8172fc72a15f4
minobol <120034142+minobol@users.noreply.github.com>
2023-09-28 13:17:27 +0300

    Update контрольная.cs

--- контрольная.cs
+++ контрольная.cs
@@ -1,31 +1,42 @@
-int[] filterEvens(int[] array)
+Console.Write(«Введите количество элементов массива: «);
+int m = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [m];
+void array(string [] stringArray)
 {
-    int[] result = { };
-    int[] tmpArray = new int[10];
-    int tal = 0;
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] % 2 != 0)
-        {
-            continue;
-        }
-        tmpArray[tal++] = array[i];
-        if (tal >= tmpArray.Length)
-        {
-            int oldSize = result.Length;
-            Array.Resize(ref result, oldSize + tal);
-            Array.ConstrainedCopy(tmpArray, 0, result, oldSize, tal);
-            tal = 0;
-        }
-    }
-
-    if (tal > 0)
-    {
-        int oldSize = result.Length;
-        Array.Resize(ref result, oldSize + tal);
-        Array.ConstrainedCopy(tmpArray, 0, result, oldSize, tal);
-    }
-
-    return result;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($»Введите {i+1} элемент массива»);
+     stringArray[i] = Console.ReadLine();
+  }
 }
+string [] symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+void printA(string [] stringArray)
+{
+    Console.Write(«[«);
+    for (int i = 0;i<stringArray.Length;i++)
+    {
+    Console.Write($»‘{stringArray[i]}’, «);
+    }
+    Console.Write(«]»);
+}
+array(stringArray);
+printA(symbol(stringArray));
